9f432cec9ca9daf05cdcf209d8a5c8e85df1511d
Siddhant Birhade <siddhantbirhade@gmail.com>
2022-05-18 00:21:18 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+function insert(num){
+  document.form.textview.value = document.form.textview.value + num
+}
+function back(){
+  var expr = document.form.textview.value;
+  document.form.textview.value = expr.substring(0,expr.length-1)
+}
+function clean(){
+  document.form.textview.value = ''
+}
+function equal(){
+  var expr = document.form.textview.value;
+  document.form.textview.value = eval(expr)
+}
